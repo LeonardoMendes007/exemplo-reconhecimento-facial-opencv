9dd50c5d0255c79cb070aef23b2c0a5c972860a2
LeonardoMendes007 <mendesleo98@gmail.com>
2023-04-04 03:01:53 -0300

    Subindo Projeto

---
+++ ReconhecimentoFacialEmFoto/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenCvSharp;
+
+var classifier = new CascadeClassifier("Resources/haarcascade_frontalface_default.xml");
+
+var window = new Window("Face Detection");
+
+var frame = Cv2.ImRead("Resources/foto_1.jpg");
+
+var gray = new Mat();
+Cv2.CvtColor(frame, gray, ColorConversionCodes.BGR2GRAY);
+
+var faces = classifier.DetectMultiScale(gray);
+
+foreach (var face in faces)
+{
+    Cv2.Rectangle(frame, face, Scalar.Red, 2);
+}
+
+while (true)
+{
+    window.ShowImage(frame);
+    int key = Cv2.WaitKeyEx(1);
+    if(key == 'q')
+    {
+        break;
+    }
+}
+
+window.Close();
---
+++ ReconhecimentoFacialEmVideo/Program.cs
@@ -0,0 +1,38 @@
+﻿using OpenCvSharp;
+
+using (var videoCapture = new VideoCapture("Resources/video.mp4"))
+{
+    var classifier = new CascadeClassifier("Resources/haarcascade_frontalface_default.xml");
+
+    var window = new Window("Face Detection");
+
+    var frame = new Mat();
+
+    while (true)
+    {
+        videoCapture.Read(frame);
+
+        if (frame.Empty())
+        {
+            break;
+        }
+
+        var gray = new Mat();
+        Cv2.CvtColor(frame, gray, ColorConversionCodes.BGR2GRAY);
+
+        var faces = classifier.DetectMultiScale(gray);
+
+        foreach (var face in faces)
+        {
+            Cv2.Rectangle(frame, face, Scalar.Red, 2);
+        }
+
+        window.ShowImage(frame);
+        int key = Cv2.WaitKeyEx(1);
+        if (key == 'q')
+        {
+            break;
+        }
+    }
+}
+
